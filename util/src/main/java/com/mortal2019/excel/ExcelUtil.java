63c65d2b5bd87e9379d2b574ccb070150350061e
wuyiyuan <619993169@qq.com>
2023-04-24 09:38:50 +0800

    feat: excel工具

---
+++ util/src/main/java/com/mortal2019/excel/ExcelUtil.java
@@ -0,0 +1,99 @@
+package com.mortal2019.excel;
+
+import org.apache.poi.hssf.usermodel.*;
+
+import java.util.List;
+
+/**
+ * @author wuyiyuan
+ * @version v1.0.0.0
+ * Created DateTime 2023/4/24 9:32
+ */
+public class ExcelUtil {
+    /**
+     * 导出Excel
+     *
+     * @param sheetName sheet名称
+     * @param title     标题
+     * @param values    内容
+     * @param wb        HSSFWorkbook对象
+     * @return HSSFWorkbook
+     */
+    public static HSSFWorkbook getWorkbook(String sheetName, String[] title, String[][] values, HSSFWorkbook wb) {
+
+        // 第一步，创建一个HSSFWorkbook，对应一个Excel文件
+        if (wb == null) {
+            wb = new HSSFWorkbook();
+        }
+
+        // 第二步，在workbook中添加一个sheet,对应Excel文件中的sheet
+        HSSFSheet sheet = wb.createSheet(sheetName);
+
+        // 第三步，在sheet中添加表头第0行,注意老版本poi对Excel的行数列数有限制
+        HSSFRow row = sheet.createRow(0);
+
+        // 第四步，创建单元格，并设置值表头 设置表头居中
+        HSSFCellStyle style = wb.createCellStyle();
+        // 创建一个居中格式
+        style.setAlignment(HSSFCellStyle.ALIGN_CENTER);
+
+        //声明列对象
+        HSSFCell cell;
+
+        //创建标题
+        for (int i = 0; i < title.length; i++) {
+            cell = row.createCell(i);
+            cell.setCellValue(title[i]);
+            cell.setCellStyle(style);
+        }
+
+        //创建内容
+        for (int i = 0; i < values.length; i++) {
+            row = sheet.createRow(i + 1);
+            for (int j = 0; j < values[i].length; j++) {
+                //将内容按顺序赋给对应的列对象
+                row.createCell(j).setCellValue(values[i][j]);
+            }
+        }
+        return wb;
+    }
+
+    public static HSSFWorkbook getWorkbook(String sheetName, String[] title, List<String[]> values, HSSFWorkbook wb) {
+
+        // 第一步，创建一个HSSFWorkbook，对应一个Excel文件
+        if (wb == null) {
+            wb = new HSSFWorkbook();
+        }
+
+        // 第二步，在workbook中添加一个sheet,对应Excel文件中的sheet
+        HSSFSheet sheet = wb.createSheet(sheetName);
+
+        // 第三步，在sheet中添加表头第0行,注意老版本poi对Excel的行数列数有限制
+        HSSFRow row = sheet.createRow(0);
+
+        // 第四步，创建单元格，并设置值表头 设置表头居中
+        HSSFCellStyle style = wb.createCellStyle();
+        // 创建一个居中格式
+        style.setAlignment(HSSFCellStyle.ALIGN_CENTER);
+
+        //声明列对象
+        HSSFCell cell;
+
+        //创建标题
+        for (int i = 0; i < title.length; i++) {
+            cell = row.createCell(i);
+            cell.setCellValue(title[i]);
+            cell.setCellStyle(style);
+        }
+
+        //创建内容
+        for (int i = 0; i < values.size(); i++) {
+            row = sheet.createRow(i + 1);
+            for (int j = 0; j < values.get(i).length; j++) {
+                //将内容按顺序赋给对应的列对象
+                row.createCell(j).setCellValue(values.get(i)[j]);
+            }
+        }
+        return wb;
+    }
+}
